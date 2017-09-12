b55cf090ab1c58875916b84c748e1580c17596fb
Jacob Williams <ponyboy47@gmail.com>
2017-09-12 08:55:01 -0600

    Renamed to Cinotify

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "inotify",
+    name: "Cinotify",
     dependencies: [
         .Package(url: "https://github.com/Ponyboy47/CSelect.git", majorVersion: 1)
     ]
